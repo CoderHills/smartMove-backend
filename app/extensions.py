d72dc2461b60c56e33270a60564b750cc405b696
Hilary Atingo <coderhilary@gmail.com>
2026-02-11 23:49:35 +0300

    Fix CORS OPTIONS preflight requests

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 migrate = Migrate()
-cors = CORS(supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(supports_credentials=True, resources={r"/api/*": {"origins": "*"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 bcrypt = Bcrypt()
