03e1969c92998deb21851847165f58a7a39d937d
Hilary Atingo <coderhilary@gmail.com>
2026-02-12 06:15:10 +0300

    Ensure migrations always run

--- start.sh
+++ start.sh
@@ -4,14 +4,13 @@ set -e
 # Set Flask app for CLI commands
 export FLASK_APP=wsgi:app
 
-# Stamp database to current revision to skip old broken migrations
-flask db stamp cc862af7b8f7
+# Try to stamp, if it fails (new DB), just continue
+flask db stamp cc862af7b8f7 || true
 
-# Run any pending migrations
+# Always run migrations to create tables
 flask db upgrade
 
-# Start Gunicorn with config file
-# PORT is provided by Render/Heroku, fallback to 8000 for local
+# Start Gunicorn
 exec gunicorn wsgi:app \
     --bind 0.0.0.0:${PORT:-8000} \
     --workers ${GUNICORN_WORKERS:-2} \
